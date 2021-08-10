b7ef18f3cb5a217d341ffa50d4b3bc60eb1832e0
reiyayakko <reiyayakko@gmail.com>
2021-08-10 22:37:52 +0900

    feat: add core methods

--- src/parser.ts
+++ src/parser.ts
@@ -1,5 +1,5 @@
 import { isArrayLike } from "emnorst";
-import { ParseState, Success, succInit, Target } from "./state";
+import { margeFail, ParseState, Success, succInit, succUpdate, Target } from "./state";
 
 type ParseRunner<T, U> = (this: void, state: Success<T>) => ParseState<U>;
 
@@ -12,5 +12,41 @@ export class Parser<T> {
         const initState = succInit(target);
         const finalState = this.run(initState);
         return finalState;
+    }
+    map<U>(this: Parser<T>, f: (t: T) => U): Parser<U> {
+        return new Parser(state => {
+            const newState = this.run(state);
+            return newState.succ ? succUpdate(newState, f(newState.value), 0) : newState;
+        });
+    }
+    flatMap<U>(this: Parser<T>, f: (r: T) => Parser<U>): Parser<U> {
+        return new Parser(state => {
+            const newState = this.run(state);
+            return newState.succ ? f(newState.value).run(newState) : newState;
+        });
+    }
+    right<U>(this: Parser<T>, parser: Parser<U>): Parser<U> {
+        return new Parser(state => {
+            const newState = this.run(state);
+            return newState.succ ? parser.run(newState) : newState;
+        });
+    }
+    left(this: Parser<T>, parser: Parser<unknown>): Parser<T> {
+        return new Parser(state => {
+            const newStateA = this.run(state);
+            if(!newStateA.succ) return newStateA;
+            const newStateB = parser.run(newStateA);
+            if(!newStateB.succ) return newStateB;
+            return succUpdate(newStateB, newStateA.value, 0);
+        });
+    }
+    or<U>(this: Parser<T>, parser: Parser<U>): Parser<T | U> {
+        return new Parser<T | U>(state => {
+            const newStateA = this.run(state);
+            if(newStateA.succ) return newStateA;
+            const newStateB = parser.run(state);
+            if(newStateB.succ) return newStateB;
+            return margeFail(newStateA, newStateB);
+        });
     }
 }
