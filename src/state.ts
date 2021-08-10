036fb5dde97d1f03cd90c51d77598c754e4d0f84
reiyayakko <reiyayakko@gmail.com>
2021-08-10 21:59:17 +0900

    feat: add function to handle Failure

--- src/state.ts
+++ src/state.ts
@@ -38,3 +38,23 @@ export interface Failure {
     readonly target: Target;
     readonly pos: number;
 }
+
+export const failFromSucc = (succ: Success<unknown>): Failure => ({
+    succ: false,
+    target: succ.target,
+    pos: succ.pos,
+});
+
+export const margeFail = (failA: Failure, failB: Failure): Failure => {
+    if(failA.target !== failB.target) {
+        throw new Error("`Failure` with different targets cannot be merged.");
+    }
+
+    if(failA.pos < failB.pos) return failA;
+    if(failA.pos > failB.pos) return failB;
+    return {
+        succ: false,
+        target: failA.target,
+        pos: failA.pos,
+    };
+};
