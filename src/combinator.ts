0c9c394f64e7a7d8ef3ca724dcebe219937425d6
reiyayakko <reiyayakko@gmail.com>
2021-08-12 17:48:00 +0900

    feat: add seq

--- src/combinator.ts
+++ src/combinator.ts
@@ -1,5 +1,19 @@
 import { Parser } from "./parser";
-import { failFromSucc, margeFail } from "./state";
+import { failFromSucc, margeFail, succUpdate } from "./state";
+
+export const seq = <T>(parsers: Parser<T>[]): Parser<T[]> =>
+    new Parser(state => {
+        const accum: T[] = [];
+        for(let i = 0; i < parsers.length; i++) {
+            const newState = parsers[i].run(state);
+            if(!newState.succ) {
+                return newState;
+            }
+            accum.push(newState.value);
+            state = newState;
+        }
+        return succUpdate(state, accum, 0);
+    });
 
 export const choice = <T>(parsers: Parser<T>[]): Parser<T> =>
     new Parser(state => {
