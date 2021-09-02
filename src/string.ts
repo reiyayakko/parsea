aaa723e37669cc8ce0cec8c4610e29bdb48b3e6a
rizzzse <rizzzse@gmail.com>
2021-09-02 22:20:50 +0900

    feat: disable all properties except "groups"

--- src/string.ts
+++ src/string.ts
@@ -1,7 +1,11 @@
 import { Parser } from "./parser";
 import { failFromSucc, succUpdate } from "./state";
 
-export const regexGroup = (re: RegExp): Parser<RegExpExecArray> => {
+export interface RegExpGroupArray extends Array<string> {
+    groups?: RegExpExecArray["groups"];
+}
+
+export const regexGroup = (re: RegExp): Parser<RegExpGroupArray> => {
     const fixedRegex = new RegExp("^" + re.source, re.flags.replace("g", ""));
 
     return new Parser(state => {
