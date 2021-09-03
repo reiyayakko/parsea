095cceadf7b32f5ac111602442d59cb50422866c
rizzzse <rizzzse@gmail.com>
2021-09-03 15:27:45 +0900

    test: change custom matcher

--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -3,31 +3,20 @@ import { el } from "../src/primitive";
 describe("Parser", () => {
     describe("many", () => {
         test("min", () => {
-            expect(el(1).many()).parseResultToEqual(
-                { succ: true, target: [], pos: 0, value: [] },
-            );
-            expect(el(1).many({ min: 2 })).parseResultToEqual(
-                { succ: true, target: [1, 1], pos: 2, value: [1, 1] },
-            );
-            expect(el(1).many({ min: 2 })).parseResultToEqual(
-                { succ: false, target: [1, "1"], pos: 1 },
-            );
-            expect(el(1).many({ min: 2 })).parseResultToEqual(
-                { succ: false, target: [1], pos: 1 },
-            );
+            expect(el(1).many()).parseToSucc([], 0, []);
+
+            const parser = el(1).many({ min: 2 });
+            expect(parser).parseToSucc([1, 1], 2, [1, 1]);
+            expect(parser).parseToFail([1, "1"], 1);
+            expect(parser).parseToFail([1], 1);
         });
         test("max", () => {
-            expect(el(1).many({ max: 2 })).parseResultToEqual(
-                { succ: true, target: [1, 1, 1, 1], pos: 2, value: [1, 1] },
-            );
-            expect(el(1).many({ max: 2 })).parseResultToEqual(
-                { succ: true, target: [1], pos: 1, value: [1] },
-            );
+            const parser = el(1).many({ max: 2 });
+            expect(parser).parseToSucc([1, 1, 1, 1], 2, [1, 1]);
+            expect(parser).parseToSucc([1], 1, [1]);
         });
         test("min <= max", () => {
-            expect(el(1).many({ min: 3, max: 1 })).parseResultToEqual(
-                { succ: true, target: [1, 1, 1, 1], pos: 3, value: [1, 1, 1] },
-            );
+            expect(el(1).many({ min: 3, max: 1 })).parseToSucc([1, 1, 1, 1], 3, [1, 1, 1]);
         });
     });
 });
--- tests/setup.ts
+++ tests/setup.ts
@@ -1,18 +1,24 @@
-import { ParseState, Parser } from "../src";
+import { Parser } from "../src/parser";
+import { ParseState, succUpdate, succInit, Target, failFrom } from "../src/state";
+
+type InferFromParser<T> = T extends Parser<infer U> ? U : never;
 
 declare global {
     namespace jest {
         interface Matchers<R, T> {
-            parseResultToEqual(result: ParseState<T extends Parser<infer U> ? U : never>): R;
+            parseToEqual(result: ParseState<InferFromParser<T>>): R;
+            parseToSucc(target: Target, pos: number, value: InferFromParser<T>): R;
+            parseToFail(target: Target, pos: number): R;
         }
     }
 }
 
-const parseResultToEqual: jest.CustomMatcher = function(
+const parseToEqual = function(
+    this: jest.MatcherContext,
     receivedParser: unknown,
     result: ParseState<unknown>,
-) {
-    const matcherName = "parseResultToEqual";
+    matcherName: string,
+): jest.CustomMatcherResult {
     const options: jest.MatcherHintOptions = {
         isNot: this.isNot,
         promise: this.promise,
@@ -43,4 +49,33 @@ const parseResultToEqual: jest.CustomMatcher = function(
     return { pass, message };
 };
 
-expect.extend({ parseResultToEqual });
+expect.extend({
+    parseToEqual(receivedParser: unknown, result: ParseState<unknown>) {
+        return parseToEqual.call(this, receivedParser, result, "parseToEqual");
+    },
+    parseToSucc(
+        receivedParser: unknown,
+        target: Target,
+        pos: number,
+        value: unknown,
+    ) {
+        return parseToEqual.call(
+            this,
+            receivedParser,
+            succUpdate(succInit(target), value, pos),
+            "parseToSucc",
+        );
+    },
+    parseToFail(
+        receivedParser: unknown,
+        target: Target,
+        pos: number,
+    ) {
+        return parseToEqual.call(
+            this,
+            receivedParser,
+            failFrom(target, pos),
+            "parseToFail",
+        );
+    },
+});
