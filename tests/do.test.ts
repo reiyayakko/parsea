ab296b2e511c67ce6f14581ebc69d1982fe6a848
rizzzse <rizzzse@gmail.com>
2021-09-13 12:54:01 +0900

    test: fix pos

--- tests/do.test.ts
+++ tests/do.test.ts
@@ -8,6 +8,6 @@ test("do", () => {
         const b = perform(satisfy<string>(el => typeof el === "string"));
         return { a, b };
     });
-    expect(parser).parseToSucc([5, "*"], 3, { a: 5, b: "*" });
+    expect(parser).parseToSucc([5, "*"], 2, { a: 5, b: "*" });
     expect(parser).parseToFail([20, 5], 1);
 });
