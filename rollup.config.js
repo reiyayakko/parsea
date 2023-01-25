ea79b55cfb7c4d2f8439e67054bf422018c819dc
rizzzse <rizzzse@gmail.com>
2023-01-26 00:40:05 +0900

    chore: banner消す

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,15 @@
 // @ts-check
 
 import ts from "@wessberg/rollup-plugin-ts";
-import pkg from "./package.json";
 
 const DEV = process.env.BUILD === "development";
-const banner = `
-/**
- * @license
- * parsea v${pkg.version}
- * Copyright 2021 rizzzse
- * License MIT
- */
-`;
 
 /** @type {import("rollup").RollupOptions} */
 const config = {
     input: "./src/index.ts",
     output: [
-        { file: "dist/parsea.esm.js", format: "esm", banner },
-        { file: "dist/parsea.cjs.js", format: "cjs", banner },
+        { file: "dist/parsea.esm.js", format: "esm" },
+        { file: "dist/parsea.cjs.js", format: "cjs" },
     ],
     external: "emnorst",
     plugins: [
