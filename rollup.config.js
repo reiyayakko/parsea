9b496dbf01c09f57359b798959198aec20e19694
rizzzse <rizzzse@gmail.com>
2023-01-27 07:03:13 +0900

    chore: use defineConfig

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,16 @@
 // @ts-check
 
+import { defineConfig } from "rollup";
 import ts from "rollup-plugin-ts";
 
 const DEV = process.env.BUILD === "development";
 
-/** @type {import("rollup").RollupOptions} */
-const config = {
+export default defineConfig({
     input: "./src/index.ts",
     output: [
         { file: "dist/parsea.esm.js", format: "esm" },
         { file: "dist/parsea.cjs.js", format: "cjs" },
     ],
     external: "emnorst",
-    plugins: [
-        ts({ transpileOnly: DEV }),
-    ],
-};
-
-export default config;
+    plugins: [ts({ transpileOnly: DEV })],
+});
