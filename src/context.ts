19d00d73ff58c6e4b14ef88c1d7b3ab3c1d7fd46
rizzzse <rizzzse@gmail.com>
2023-06-20 16:54:14 +0900

    add label

--- src/context.ts
+++ src/context.ts
@@ -26,4 +26,37 @@ export class Context {
             this.errors.push(error);
         }
     }
+    // @internal
+    group(): [number, number] {
+        return [this.errorIndex, this.errors.length];
+    }
+    // @internal
+    length([startErrorIndex, startErrorLength]: [number, number]): number {
+        return (
+            this.errors.length -
+            (startErrorIndex !== this.errorIndex ? 0 : startErrorLength)
+        );
+    }
+    makeError() {
+        const errors = [];
+        for (const error of this.errors) {
+            if (error.type === "Label") {
+                const labelStart = errors.length - labelLength(errors, error.length);
+                errors.splice(labelStart);
+            }
+            errors.push(error);
+        }
+        return { index: this.errorIndex, errors };
+    }
 }
+
+const labelLength = (errors: readonly ParseError[], length: number): number => {
+    let accum = 0;
+    let count = 0;
+    for (const error of errors.slice().reverse()) {
+        accum += error.type === "Label" ? error.length - 1 : 1;
+        count++;
+        if (accum >= length) break;
+    }
+    return count;
+};
--- src/error.ts
+++ src/error.ts
@@ -2,11 +2,18 @@ import type { Source } from "./context";
 
 export type ParseError =
     | { type: "Unknown" }
-    | { type: "Expected"; value: Source };
+    | { type: "Expected"; value: Source }
+    | { type: "Label"; name: string; length: number };
 
 export const unknown: ParseError = { type: "Unknown" };
 
 export const expected = (value: Source): ParseError => ({
     type: "Expected",
     value,
+});
+
+export const label = (name: string, length = 0): ParseError => ({
+    type: "Label",
+    name,
+    length,
 });
