9e2daa5217af6015f32679ba64b1101df5096e3d
uzmoi <rizzzse@gmail.com>
2024-01-14 19:57:12 +0900

    expose `Source` type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./combinator";
 export * from "./do";
-export type { Config, ParseResult, Parsed, Parser } from "./internal";
+export type { Config, ParseResult, Parsed, Parser, Source } from "./internal";
 export * from "./primitive";
 export * from "./string";
