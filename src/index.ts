7ae68f32b416b3ebcdb0b1a80054e1ce7c0225fd
uzmoi <rizzzse@gmail.com>
2024-09-19 16:13:49 +0900

    add parseA function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from "./combinator";
 export * from "./do";
 export type { Config, ParseResult, Parsed, Parser, Source } from "./internal";
+export * from "./parsea";
 export * from "./primitive";
 export * from "./string";
